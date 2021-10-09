dd8ce49cc2473c1899a716651fc2b672867f6c52
Giacomo Trezzi <giacomo.trezzi@gmail.com>
2021-10-09 10:42:09 +0200

    mostro dettagli scansione

--- validatorServer/app.js
+++ validatorServer/app.js
@@ -15,7 +15,7 @@ const urlUpdate = "https://get.dgc.gov.it/v1/dgc/signercertificate/update";
 const urlStatus = "https://get.dgc.gov.it/v1/dgc/signercertificate/status";
 const urlSettings = "https://get.dgc.gov.it/v1/dgc/settings";
 
-const ADD_HOLDER_DETAILS = false;
+const ADD_HOLDER_DETAILS = true;
 
 let validKids;
 let signerCertificates;
@@ -28,31 +28,31 @@ const updateCertificates = (async () => {
 	// get the list of valid KIDs
 	response = await fetch(urlStatus);
 	validKids = await response.json();
-	
+
 	console.log(validKids.length + " valid KIDs downloaded" );
-	
+
 	// get the list of certificates
 	signerCertificates = [];
 	certificateDownloadedCount = 0;
-	certificateAddedCount = 0;					  
+	certificateAddedCount = 0;
 	let headers = {};
 	const httpsAgent = new https.Agent({ keepAlive: true });
 	do {
-		
+
 		response = await fetch(urlUpdate, {
 			headers,
 			httpsAgent
 		})
-		
+
 		headers = {'X-RESUME-TOKEN' : response.headers.get('X-RESUME-TOKEN')};
 		const certificateKid = response.headers.get('X-KID');
 		const certificate = await response.text();
-		
+
 		// a certificate has been downloaded
 		if(certificate) {
-			
+
 			certificateDownloadedCount++;
-			
+
 			// the certificate is valid, add it to the list
 			if(validKids.includes(certificateKid)) {
 				certificateAddedCount++;
@@ -69,7 +69,7 @@ const updateSettings = (async () => {
 
 	response = await fetch(urlSettings);
 	settings = await response.json();
-	
+
 	console.log("Done");
 });
 
@@ -80,43 +80,43 @@ const main = (async () => {
 
 	const server = http.createServer();
 	server.on('request', async (req, res) => {
-		
+
 		// set CORS header to allow browser clients
 		res.setHeader('Access-Control-Allow-Origin', '*');
-		
+
 		const dgc = url.parse(req.url, true).query.dgc;
-		
+
 		if(dgc === undefined) {
 			res.statusCode = 400;
 			res.setHeader('Content-Type', 'text/plain');
 			res.end("Invalid DGC");
 		}
 		else {
-			
+
 			// init DCC library
 			let dcc;
 			try {
 				dcc = await DCC.fromRaw(dgc);
-			
+
 			// error when decoding DGC
 			} catch (e) {
-			
+
 				res.statusCode = 400;
 				res.setHeader('Content-Type', 'text/plain');
 				res.end("INVALID: " + e.message);
-				return;		 
+				return;
 			}
-			
+
 			// check DGC signature
 			let signatureVerified = false;
 			for(let certificate of signerCertificates) {
-							
+
 				try {
-					
+
 					// get key and jwk from certificate
 					key = rs.KEYUTIL.getKey(certificate);
 					jwk = rs.KEYUTIL.getJWKFromKey(key);
-					
+
 					// EC key, the library expects x and y coordinates as hex strings
 					if(jwk.kty == 'EC') {
 						verifier = {
@@ -124,7 +124,7 @@ const main = (async () => {
 							y: Buffer.from(jwk.y, 'base64').toString('hex')
 						};
 					}
-					
+
 					// RSA key, the library expects modulus and exponent as Buffers
 					else if(jwk.kty == 'RSA') {
 						verifier = {
@@ -132,47 +132,47 @@ const main = (async () => {
 							e: Buffer.from(jwk.e, 'base64')
 						};
 					}
-					
+
 					signatureVerified = await dcc.checkSignature(verifier);
 				} catch {}
 				if(signatureVerified) break;
 			}
-			
+
 			// no signer certificate found
 			if(!signatureVerified) {
-			
+
 				res.statusCode = 400;
 				res.setHeader('Content-Type', 'text/plain');
 				res.end("INVALID: signature");
-				return;					
+				return;
 			}
-			
+
 			// check DGC content
 			let validate;
-			
+
 			// 1. vaccination
 			if(dcc.payload.v) validate = vaccination.validateVaccination(settings, dcc);
-			
+
 			// 2. test
 			if(dcc.payload.t) validate = test.validateTest(settings, dcc);
-			
+
 			// 3. recovery
 			if(dcc.payload.r) validate = recovery.validateRecovery(settings, dcc);
-			
+
 			// Add holder details if required
 			let response;
 			if(ADD_HOLDER_DETAILS) {
-				
+
 				let surname = dcc.payload.nam.fn;
 				let forename = dcc.payload.nam.gn;
 				let dob = dcc.payload.dob;
 				response = validate.message + " - " + surname + " " + forename + " (" + dob + ")";
 			} else response = validate.message;
-						
+
 			if(validate.result) res.statusCode = 200;
 			else res.statusCode = 400;
 			res.setHeader('Content-Type', 'text/plain');
-			res.end(response);				
+			res.end(response);
 		}
 	});
 
